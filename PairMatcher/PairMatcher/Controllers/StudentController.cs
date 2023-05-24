00ca3791fa086cf083ec9fc572b4baecc679ee95
TAHiRLI <tahrli.tahirr@gmail.com>
2023-05-24 16:34:45 +0400

    Student add section complete Closes #5

--- PairMatcher/PairMatcher/Controllers/StudentController.cs
+++ PairMatcher/PairMatcher/Controllers/StudentController.cs
@@ -1,12 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PairMatcher.DAL;
+using PairMatcher.ViewModels;
 
 namespace PairMatcher.Controllers
 {
+    // Content
+    //
+    // 1. Add Student
+
     public class StudentController : Controller
     {
+        private readonly PairMatcherDbContext _context;
+
+        public StudentController(PairMatcherDbContext context)
+        {
+            _context = context;
+        }
+
+        // =============================================
+        // 1. Add Student
+        // =============================================
+
         public IActionResult AddStudent()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult AddStudent(StudentAddVM studentVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Student student = new Student()
+            {
+                Name = studentVM.Name,
+                Gender = studentVM.Gender,
+                SameGenderMatch = studentVM.SameGenderMatch,
+            };
+
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+            return RedirectToAction("Students","Student");
+        }
+        // =============================================
+        // 1.  Students
+        // =============================================
+
+        public IActionResult Students()
+        {
+            var students = _context.Students.ToList() ;
+
+            return View(students);
+        }
+
     }
 }
--- PairMatcher/PairMatcher/ViewModels/StudentAddVM.cs
+++ PairMatcher/PairMatcher/ViewModels/StudentAddVM.cs
@@ -1,8 +1,12 @@
-﻿namespace PairMatcher.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PairMatcher.ViewModels
 {
     public class StudentAddVM
     {
+        [MaxLength(50)]
         public string Name { get; set; }
         public bool Gender { get; set; }
+        public bool SameGenderMatch { get; set; }
     }
 }
